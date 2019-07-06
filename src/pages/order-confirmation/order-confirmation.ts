1efbf54d1e904ea60e63565717f5d175a5677a20
Feliphe Marinho <feliphemarinho18@gmail.com>
2019-07-06 13:54:14 -0300

    Informando pedido registrado ao usuario

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -20,6 +20,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codpedido: string;
 
   constructor(
       public navCtrl: NavController
@@ -58,7 +59,7 @@ export class OrderConfirmationPage {
         .insert(this.pedido)
         .subscribe(response => {
           this.cartService.createOrClearCart();
-          console.log(response.headers.get('location'));
+          this.codpedido = this.extractId(response.headers.get('location'));
         },
         error => {
           if(error.status == 403) {
@@ -69,5 +70,14 @@ export class OrderConfirmationPage {
 
   back() {
     this.navCtrl.setRoot('CartPage');
+  }
+
+  private extractId(location: string) : string {
+    let position = location.lastIndexOf('/');
+    return location.substring(position + 1, location.length);
+  }
+
+  home() {
+    this.navCtrl.setRoot('CategoriasPage');
   }
 }
